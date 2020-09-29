1531a7bc887fcbbf6374e2980b6d7238ef66535c
PhirosWolf <phiroswolf@tuta.io>
2020-09-29 16:10:27 +0200

    :white_check_mark: Updated basic engine tests; :sparkles: :construction: Created unit-structs (Board, Game, Piece, ...)
    Signed-off-by: PhirosWolf <phiroswolf@tuta.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
 // The ~engine~ (the thing that handles rules & stuff)
+
+pub struct Board;
+pub struct Piece;
+pub struct Rule;
+pub struct Game;
--- tests/test.rs
+++ tests/test.rs
@@ -1,39 +1,39 @@
 // The ~tests~ (the thing that tests functions & stuff)
 
-use sharmat::Board;
+use sharmat::*;
 
 // Engine basic tests
 
 #[test]
 fn board_create() {
-    let board = Board::new();
+    let board = Board::new(9, 8);
 }
 
 #[test]
 fn board_set_piece() {
-    let mut board = Board::new();
+    let mut board = Board::new(9, 8);
     board.set(Piece::new(), 0, 0);
 }
 
 #[test]
 fn board_get_piece() {
-    let mut board = Board::new();
-    let piece = Piece::new();
+    let mut board = Board::new(9, 8);
+    let piece = Piece::new(/* UNKNOWN */);
     board.set(piece.clone(), 0, 0);
     assert_eq!(board.get(0, 0), piece);
 }
 
 #[test]
 fn piece_create() {
-    let piece = Piece::new();
+    let piece = Piece::new(/* UNKNOWN */);
 }
 
 #[test]
 fn rule_create() {
-    let rule = Rule::new();
+    let rule = Rule::new(/* UNKNOWN */);
 }
 
 #[test]
 fn game_create() {
-    let game = Game::new();
+    let game = Game::new(/* UNKNOWN */);
 }
